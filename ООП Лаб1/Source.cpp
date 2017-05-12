5826234701013dc0d5ffa2415d154cf2dbbae20e
YanaYakovleva <YanaSergevna@bk.ru>
2017-05-12 11:57:32 +0700

    Add

--- ООП Лаб1/Source.cpp
+++ ООП Лаб1/Source.cpp
@@ -3,10 +3,28 @@ using namespace std;
 
 void main()
 {
+	system("color F0");
 	String a("abc");
-	String b("cde");
-	String c = a + b;
-	c.show();
+	a.show();
+	cout << "Count = " << String::getCount() << endl;
+	a.setString("AbC deF");
+	cout << "String after change" << endl;
+	a.show();
+	int index = a.search("de");
+	cout << "First occurrrence index \"de\"= " << index << endl;
+	int length = a.length();
+	cout << "Length string = " << length << endl;
+	String b = a.concat("ghk");
+	cout << "String before concatenation: " << endl << a.c_str() << endl;
+	cout << "String after concatenation with \"ghk\": " << endl << b.c_str() << endl;
+	cout << "Get symbol #2:" << a.getSymbol(2) << endl;
+	String c = a.to_apper();
+	String d = a.to_lower();
+	cout << "String a: " << endl << a.c_str() << endl;
+	cout << "String after to_apper: " << endl << c.c_str() << endl;
+	cout << "String after to_lower: " << endl << d.c_str() << endl;
+	String e = a.replace("bC", "NNN");
+	cout << "String after replace \"bC\" to \"NNN\": " << endl << e.c_str() << endl;
 
 	system("pause");
 }
